c56b06601f451440e37be40d54b2f79e36719e08
dlopez13 <steven97_lop@hotmail.com>
2025-06-07 22:28:51 -0500

    Updates

---
+++ _next/static/mZR9gesKwRPw-peElPyYa/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcurriculum\u002F[unitId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- src/hooks/useAuth.ts
+++
@@ -1,2 +0,0 @@
-"use client";
-export { useAuth } from '@/components/auth/AuthProvider';
